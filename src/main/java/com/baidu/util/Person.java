63f3340d1461695a54209b49215ab48b69f61af0
muzhenhua <493688171@qq.com>
2017-08-29 13:31:16 +0800

    第二次提交

---
+++ src/main/java/com/baidu/util/Person.java
@@ -0,0 +1,15 @@
+package com.baidu.util;
+
+public class Person {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
